293fc305c26c26332f7ef8055be6ea913b3eda56
zw67 <zhuanyan.wang@outlook.com>
2020-07-19 03:44:37 +0300

    Change to external svg assets 1. Add path alias "Assets" in webpack.common.js and tsconfig.json for demos. 2. Move testStub above the path aliases import in jest.config.js. 3. Update Notification component, tests, style.

--- demo-container-pattern/src/components/common/Notification/index.test.tsx
+++ demo-container-pattern/src/components/common/Notification/index.test.tsx
@@ -28,17 +28,19 @@ describe("Notification", () => {
   });
 
   it("should render based on props", () => {
+    // Should slide in with exclamation icon
     expect(wrapper.find("div.notification.slide-in").length).toEqual(1);
-    expect(wrapper.find("#circle-exclamation").length).toEqual(1);
+    expect(wrapper.find(".circle-exclamation").length).toEqual(1);
     expect(wrapper.find("span").text()).toEqual("Test 1.");
 
+    // Should slide out with tick icon
     wrapper = setupWrapper(setupStore(), {
       icon: "tick",
       text: "Test 2.",
       slideOut: true,
     });
     expect(wrapper.find("div.notification.slide-out").length).toEqual(1);
-    expect(wrapper.find("#circle-tick").length).toEqual(1);
+    expect(wrapper.find(".circle-tick").length).toEqual(1);
     expect(wrapper.find("span").text()).toEqual("Test 2.");
   });
 });
--- demo/src/components/common/Notification/index.tsx
+++ demo/src/components/common/Notification/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import tickIcon from "Assets/tick.svg";
+import exclamationIcon from "Assets/exclamation.svg";
+
 type propTypes = {
   icon: string;
   text: string;
@@ -16,30 +19,9 @@ const Notification: React.FC<propTypes> = ({
   // JSX elements
   let iconElement = <span></span>;
   if (icon === "exclamation") {
-    iconElement = (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 152.5 152.5"
-        fill="#ffc657"
-        id="circle-exclamation"
-      >
-        <path d="M76.249.0038a76.25,76.25,0,1,0,76.25,76.25A76.3356,76.3356,0,0,0,76.249.0038Zm0,135.5a59.25,59.25,0,1,1,59.25-59.25A59.3178,59.3178,0,0,1,76.249,135.5038Z" />
-        <rect x="67.749" y="103.4638" width="17" height="18.8" />
-        <rect x="67.749" y="30.2338" width="17" height="67.05" />
-      </svg>
-    );
+    iconElement = <img className="circle-exclamation" src={exclamationIcon} />;
   } else if (icon === "tick") {
-    iconElement = (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 152.5 152.5"
-        fill="#66bb6a"
-        id="circle-tick"
-      >
-        <path d="M76.25,0A76.25,76.25,0,1,0,152.5,76.25,76.3292,76.3292,0,0,0,76.25,0Zm0,135.5A59.25,59.25,0,1,1,135.5,76.25,59.3178,59.3178,0,0,1,76.25,135.5Z" />
-        <polygon points="116.96 56.65 65.75 107.86 35.54 77.65 47.56 65.63 65.75 83.82 104.93 44.63 116.96 56.65" />
-      </svg>
-    );
+    iconElement = <img className="circle-tick" src={tickIcon} />;
   }
 
   return (
