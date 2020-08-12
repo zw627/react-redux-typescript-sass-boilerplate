c1e2c2f6c912cb059c2b98fd95c36346548b657a
zw67 <zhuanyan.wang@outlook.com>
2020-08-12 19:47:17 +0300

    Lazy load NotFound, fix App component

--- demo-container-pattern/src/components/App.tsx
+++ demo-container-pattern/src/components/App.tsx
@@ -6,25 +6,27 @@ import store from "Store/index";
 import Home from "Components/Home";
 import NotificationStorage from "Components/NotificationStorage";
 import ThemeSwtich from "Containers/ThemeSwitch";
-import NotFound from "Components/NotFound";
-import "Styles/index.scss";
 
 const Scoreboard = React.lazy(() => import("Components/Scoreboard"));
-const App = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/scoreboard" component={Scoreboard} />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
+const NotFound = React.lazy(() => import("Components/NotFound"));
 
-    <NotificationStorage />
-    <ThemeSwtich />
-  </Provider>
-);
+const App: React.FC<Record<string, unknown>> = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/scoreboard" component={Scoreboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
+
+      <NotificationStorage />
+      <ThemeSwtich />
+    </Provider>
+  );
+};
 
 export default App;
--- demo/src/components/App.tsx
+++ demo/src/components/App.tsx
@@ -6,25 +6,27 @@ import store from "Store/index";
 import Home from "Components/Home";
 import NotificationStorage from "Components/NotificationStorage";
 import ThemeSwtich from "Components/ThemeSwitch";
-import NotFound from "Components/NotFound";
 
 const Scoreboard = React.lazy(() => import("Components/Scoreboard"));
+const NotFound = React.lazy(() => import("Components/NotFound"));
 
-const App = (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/scoreboard" component={Scoreboard} />
-          <Route component={NotFound} />
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
+const App: React.FC<Record<string, unknown>> = () => {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/scoreboard" component={Scoreboard} />
+            <Route component={NotFound} />
+          </Switch>
+        </Suspense>
+      </BrowserRouter>
 
-    <NotificationStorage />
-    <ThemeSwtich />
-  </Provider>
-);
+      <NotificationStorage />
+      <ThemeSwtich />
+    </Provider>
+  );
+};
 
 export default App;
